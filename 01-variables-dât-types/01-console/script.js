b20ee193abcfa604a5bb5e9d9f0c7c3595aaa41f
hoalt29 <149796544+hoalt29@users.noreply.github.com>
2024-01-09 14:28:27 +0700

    Add files via upload

---
+++ 01-variables-dât-types/01-console/script.js
@@ -0,0 +1,43 @@
+// // Log number
+// console.log(100);
+
+// // Log string
+// console.log('Hello World');
+
+// // Log multiple values
+// console.log(20, 'Hello', true);
+
+// // Log a variable
+// const x = 100;
+// console.log(x);
+
+// // Console error & warning
+// console.error('Alert');
+// console.warn('Warning');
+
+// // Log object as table
+// console.table({ name: 'Brad', email: 'brad@gmail.com' });
+
+// // Group console commands
+// console.group('simple');
+// console.log(x);
+// console.error('Alert');
+// console.warn('Warning');
+// console.groupEnd();
+
+// // Add CSS to logs
+// const styles = 'padding: 10px; background-color: white; color: green';
+// console.log('%cHello World', styles);
+
+//$ten_bien = ''
+
+//let hoTen = 'Hoalt';
+let firsName = 'John';
+const myName = 'Hoalt'
+const lastName = 'Lo';
+
+let age = 19;
+age = 20;
+console.log("Age", age);
+
+console.log(lastName);
